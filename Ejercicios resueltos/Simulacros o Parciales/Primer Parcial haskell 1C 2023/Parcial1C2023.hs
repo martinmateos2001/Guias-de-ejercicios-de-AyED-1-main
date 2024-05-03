078202fe2b6c0759eefdf95375f17aa553a91aa3
Martín Mateos <mmateos@eest1chivilcoy.edu.ar>
2024-05-03 20:37:21 -0300

    Me habia olvidado de sumar en el paso recursivo de numElemLista

--- Ejercicios resueltos/Simulacros o Parciales/Primer Parcial haskell 1C 2023/Parcial1C2023.hs
+++ Ejercicios resueltos/Simulacros o Parciales/Primer Parcial haskell 1C 2023/Parcial1C2023.hs
@@ -1,7 +1,7 @@
 module Parcial1C2023 where
 --1.1
-votosEnBlanco :: [(String,String)] -> [Int] -> Int -> Int 
-votosEnBlanco 
+--votosEnBlanco :: [(String,String)] -> [Int] -> Int -> Int 
+--votosEnBlanco 
    
 --el numero de elementos de la lista de formulas es igual al de votos
 {-
@@ -15,15 +15,12 @@ numeroVotos (voto:votos) |(voto >= 0) && ((voto:votos) /= []) = 1 + numeroVotos(
 -}
 numElemLista :: (Eq a) => [a] -> Int
 numElemLista [] = 0
-numElemLista (elem:restoDeElementos) |(elem:restoDeElementos) /= [] = 1 numElemLista(restoDeElementos)
+numElemLista (elem:restoDeElementos) |(elem:restoDeElementos) /= [] = 1 + numElemLista(restoDeElementos)
 
 votosTotales :: [Int]->Int
 votosTotales []=0
 votosTotales (voto:restoDeVotos) = voto + votosTotales(restoDeVotos)
 
---Entonces el voto en blanco es igual votos totales menos la resta de las lista que no son ("","")
-numVotosEnBlanco :: [(String,String)]->[Int]->Int
-numVotosEnBlanco
 
 --1.2
 formulasValidas :: [(String,String)] -> Bool -- evaluo si cada formula de la lista es formula valida.
