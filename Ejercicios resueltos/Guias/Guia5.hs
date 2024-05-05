ff490b12679c86a8254da428e2933e51a5c067f7
Martín Mateos <mmateos@eest1chivilcoy.edu.ar>
2024-05-05 15:05:46 -0300

    Hecho ejercicio 3.4, sumarN

--- Ejercicios resueltos/Guias/Guia5.hs
+++ Ejercicios resueltos/Guias/Guia5.hs
@@ -119,6 +119,15 @@ maximo (x:[]) = x -- es lo ultimo que recorre
 maximo (x:y:ys) |contarElementos (x:y:ys) >1 && x>=y = maximo (x:ys) -- x>=y me evaluo de nuevo x con la cola que se dividira en en y':ys por la forma de la funcion, que quedara en maximo x:y':ys y asi hasya el caso de arriba 
                 |contarElementos (x:y:ys) >1 && x<y = maximo (y:ys) -- si no es mayor o igual entonces es menor y quedaria maximo y:y':xs 
 
+{-3.4, dados un numero n y una lista L, devuelvo una lista L' que es igual a los elementos de L pero sumado a cada uno n
+asegura:
+*|L'| = |L|
+-}
+sumarN :: Integer -> [Integer] -> [Integer]
+sumarN n [x] = [x+n]
+sumarN n (x:xs) = ((x+n):sumarN n xs)
+
+
 {-
 Ejercicio 3.9, ordenar la lista de forma creciente (menor a mayor) | pista dada por la profesora: usar maximo y quitar.
 -}
