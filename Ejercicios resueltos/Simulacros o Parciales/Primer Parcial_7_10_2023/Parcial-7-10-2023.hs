f8646e0c34d421080850bc88642b022bef3034cd
Martín Mateos <mmateos@eest1chivilcoy.edu.ar>
2024-05-07 16:35:29 -0300

    En cantDivisores me faltaron casos por contemplar.

--- Ejercicios resueltos/Simulacros o Parciales/Primer Parcial_7_10_2023/Parcial-7-10-2023.hs
+++ Ejercicios resueltos/Simulacros o Parciales/Primer Parcial_7_10_2023/Parcial-7-10-2023.hs
@@ -1,15 +1,21 @@
 {-Primos en Cero
 Dada una secuencia s de enteros, la devuelve pero en la posicion que habia un numero primo lo cambia por un cero.-}
 
-primosEnCero :: [Int] -> [Int]
+--primosEnCero :: [Int] -> [Int]
 
 esPrimo :: Int -> Bool -- es Primo si la cantidad de divisores es igual 4 {-1, 1, -n, n}
 esPrimo n = cantDivisores n n == 4
 
 cantDivisores :: Int -> Int -> Int -- divido a n por a  y si da cero es divisor, a entre [-n, n]
 cantDivisores n a 
-    |a==(-n) = 1
-    |a==0 = cantDivisores n (-1)
+    |n>0 && a==n = 1 + cantDivisores n (a-1) -- caso inicial para n>0, n n
+    |n>0 && a==(-n) = 1 -- caso final para n>0, n -n
+    |n>0 && a==0 = cantDivisores n (-1) -- no puedo dividir por cero y lo salteo.
+    |n<0 && a==0 = cantDivisores n (1) -- no puedo dividir por cero y lo salteo.
+    |n<0 && a == n = 1 + cantDivisores n (a+1) -- caso inicial para n<0, seria -n -n
+    |n<0 && a == (-n) = 1 -- caso final para n<0, seria -n n
 cantDivisores n a 
-    |a > (-n) && a <= n && mod n a == 0 = 1 + cantDivisores n (a-1) 
-    |a > (-n) && a <= n && mod n a /= 0 = 0 + cantDivisores n (a-1)
+    |n > 0 && a > (-n) && a < n && mod n a == 0 = 1 + cantDivisores n (a-1) 
+    |n > 0 && a > (-n) && a < n && mod n a /= 0 = 0 + cantDivisores n (a-1)
+    |n < 0 && a > (-n) && a < n && mod n a == 0 = 1 + cantDivisores n (a+1)
+    |n < 0 && a > (-n) && a < n && mod n a /= 0 = 0 + cantDivisores n (a+1)
