a954b4825b2ac34a170f3e4bc16b01d5f8a71b76
Martín Mateos <mmateos@eest1chivilcoy.edu.ar>
2024-05-16 18:30:16 -0300

    ?

---
+++ Ejercicios resueltos/Python/Guias/Practica6.py
@@ -0,0 +1,9 @@
+import math
+
+# Ejercicio 1
+def imprimir_hola_mundo ():
+    print("¡Hola mundo!")
+
+def imprimir_un_verso():
+    print("hola soy Martin Mateos y aca hay un salto de linea...  \nAhora estoy escribiendo abajo y debajo hay una carita feliz\n:)")
+
