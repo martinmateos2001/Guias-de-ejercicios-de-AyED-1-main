db2a8a4aa53c23cec30c1bc9dac12e5690408856
Martín Mateos <mmateos@eest1chivilcoy.edu.ar>
2024-05-20 19:14:03 -0300

    Hay que llamar a las funciones con su nombre y parentesis al final.

--- Ejercicios resueltos/Python/Guias/Practica6.py
+++ Ejercicios resueltos/Python/Guias/Practica6.py
@@ -7,3 +7,5 @@ def imprimir_hola_mundo ():
 def imprimir_un_verso():
     print("hola soy Martin Mateos y aca hay un salto de linea...  \nAhora estoy escribiendo abajo y debajo hay una carita feliz\n:)")
 
+imprimir_hola_mundo()
+imprimir_un_verso()
