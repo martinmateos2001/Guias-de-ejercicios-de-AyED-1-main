bafb0f466c5e54e657bd326f8edab05289e8b03b
Martín Mateos <mmateos@eest1chivilcoy.edu.ar>
2024-05-20 19:35:03 -0300

    Hecho ej 1.3

--- Ejercicios resueltos/Python/Guias/Practica6.py
+++ Ejercicios resueltos/Python/Guias/Practica6.py
@@ -1,11 +1,20 @@
 import math
 
-# Ejercicio 1
-def imprimir_hola_mundo ():
+# Ejercicio 1.1
+def imprimir_hola_mundo():
     print("¡Hola mundo!")
 
+imprimir_hola_mundo()
+
+#1.2
 def imprimir_un_verso():
     print("hola soy Martin Mateos y aca hay un salto de linea...  \nAhora estoy escribiendo abajo y debajo hay una carita feliz\n:)")
 
-imprimir_hola_mundo()
 imprimir_un_verso()
+
+#1.3
+def raizDe2(x:int):
+    return round(math.sqrt(x), 4)
+
+print(raizDe2(2))
+
