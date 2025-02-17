287541e0b515f9e387c9e271281b61b887927800
Martín Mateos <mmateos@eest1chivilcoy.edu.ar>
2025-02-17 19:08:01 -0300

    ccxczc

--- Python/TestPractica.py
+++ Python/TestPractica.py
@@ -22,7 +22,26 @@ class TestDeterminante2x2 (unittest.TestCase):
         )
         self.assertEqual(res, 6)
 
+matriz = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]]
 
+class TestSubMatriz (unittest.TestCase):
+    def test1 (self):
+        resT1 = subMatriz(matriz,0,0)
+        self.assertEqual(resT1, [[5,6], [8,9]])
+    
+    def test2(self):
+        resT2 = subMatriz(matriz, 0, 1)
+        self.assertEqual(resT2, [[4,6],[7,9]])
+    
+    def test3(self):
+        resT3 = subMatriz(matriz, 0,2)
+        self.asertEqual(resT3, [[4,5], [7,8]])
+
+"""class TestDeterminanteMatriz3x3(unittest.TestCase):
+    def test"""
 
 if __name__ == '__main__':
     unittest.main()
