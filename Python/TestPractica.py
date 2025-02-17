0f5fbbd2d5990eb5c7733a7fe2dc2afb9692936f
Martín Mateos <mmateos@eest1chivilcoy.edu.ar>
2025-02-17 19:58:50 -0300

    Me comi una s en assertEqual, ahora los tests funcionan.

--- Python/TestPractica.py
+++ Python/TestPractica.py
@@ -26,8 +26,9 @@ matriz = [
         [1,2,3],
         [4,5,6],
         [7,8,9]]
-
 class TestSubMatriz (unittest.TestCase):
+    
+    
     def test1 (self):
         resT1 = subMatriz(matriz,0,0)
         self.assertEqual(resT1, [[5,6], [8,9]])
@@ -38,7 +39,7 @@ class TestSubMatriz (unittest.TestCase):
     
     def test3(self):
         resT3 = subMatriz(matriz, 0,2)
-        self.asertEqual(resT3, [[4,5], [7,8]])
+        self.assertEqual(resT3, [[4,5], [7,8]])
 
 """class TestDeterminanteMatriz3x3(unittest.TestCase):
     def test"""
